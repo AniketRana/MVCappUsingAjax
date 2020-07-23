f3e80c5768344ea5c86b9b9714141d47a6ccbbbf
Aniketrana <ranaannu93@gmail.com>
2020-07-23 17:24:52 +0530

    Add project files.

---
+++ AjaxFormWithoutRefreshInMVC/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxFormWithoutRefreshInMVC.Models;
+using Newtonsoft.Json;
+
+namespace AjaxFormWithoutRefreshInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(Employee model)
+        {
+            return Json("true", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+        public JsonResult Countries()
+        {
+            List<string> countries = new List<string>()
+            {
+                "Afghanistan",
+                "Albania",
+                "Algeria",
+                "Andorra",
+                "Angola",
+                "AntiguaandBarbuda",
+                "Argentina",
+                "Armenia",
+                "Australia",
+                "Austria",
+                "Azerbaijan"
+            };
+            var json = JsonConvert.SerializeObject(countries);
+            return Json(json,JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult States()
+        {
+            List<string> states = new List<string>()
+            {
+                "State -- Afghanistan",
+                "State -- Albania",
+                "State -- Algeria",
+                "State -- Andorra",
+                "State -- Angola",
+                "State -- AntiguaandBarbuda",
+                "State -- Argentina",
+                "State -- Armenia",
+                "State -- Australia",
+                "State -- Austria",
+                "State -- Azerbaijan"
+            };
+            var json = JsonConvert.SerializeObject(states);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult City()
+        {
+            List<string> City = new List<string>()  
+            {
+                "City -- Afghanistan",
+                "City -- Albania",
+                "City -- Algeria",
+                "City -- Andorra",
+                "City -- Angola",
+                "City -- AntiguaandBarbuda",
+                "City -- Argentina",
+                "City -- Armenia",
+                "City -- Australia",
+                "City -- Austria",
+                "City -- Azerbaijan"
+            };
+            var json = JsonConvert.SerializeObject(City);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxFormWithoutRefreshInMVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxFormWithoutRefreshInMVC.Models
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email{ get; set; } 
+    }
+}
